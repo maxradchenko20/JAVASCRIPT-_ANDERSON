9ddb219eb823158f26cd0f099aef404bd2132a48
Maxim <maxradchenko20@gmail.com>
2021-07-08 21:49:37 +0300

    'Firstttttt'

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//1
+function conwerter(firstNumber,secondNumber){
+    firstNumber = prompt('Введите число:','');
+    secondNumber = prompt('В какую систему счисления конвертируем???','');
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)){
+        console.log(parseInt(firstNumber).toString(secondNumber));
+    }else {
+        console.error("Некорректный ввод!")
+    }
+}
+conwerter();
+
+//2
+const isValidNumber = (string) => {
+    return !Number.isNaN(Number(string));
+};
+
+const aggregator1 = () => {
+    const firstNumber = prompt('first');
+    if (!isValidNumber(firstNumber)) {
+        console.error('is not a valid number');
+        return;
+    }
+    const secondNumber = prompt('second');
+    if (!isValidNumber(secondNumber)) {
+        console.error('is not a valid number');
+        return;
+    }
+
+    console.log(`Ответ: ${+firstNumber + +secondNumber},${firstNumber / secondNumber}`);
+}
+aggregator1();
